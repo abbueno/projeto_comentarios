6c01e7dc44f612f151dd608b8b78337cd6ad4b98
ALLAN BIGHI BUENO <allanbbueno@bol.com.br>
2021-01-12 11:00:58 -0300

    Criação do  Projeto
    Criação do  Projeto

---
+++ index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Sistema de Comentários</title>
+    <link rel="stylesheet" href="css/estilo.css">
+</head>
+<body>
+    <nav>
+        <ul>
+            <li><a href="">Discussão</a></li>
+            <li><a href="">Entrar</a></li>
+        </ul>    
+    </nav>
+    <h3>Conteúdo qualquer</h3>
+</body>
+</html>
